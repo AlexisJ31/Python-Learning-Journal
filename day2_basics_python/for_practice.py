4c36cb61ffa158661a613a4a9738f17902f41ab1
Alexis <alexisjaen35@gmail.com>
2026-01-25 13:54:09 -0500

    se le agrego un ejemplo mas al for_practice, donde utilizamos el ciclo for y el if

--- day2_basics_python/for_practice.py
+++ day2_basics_python/for_practice.py
@@ -9,3 +9,15 @@ for player in crack_players:
 # Esto imprimirá del 0 al 4 (el 5 no se incluye)
 for i in range(5):
     print(f"Vuelta número: {i}")
+
+#Ejemplo 3
+#utilizare el ciclo for y el if para este ejercicio
+#definimos la lista
+marcas = ["Smansung", "Nokia", "Sony", "Apple", "Huawei"]
+#recorremos la lista con un for
+for marca in marcas:
+    #usamos un if para verificar si la marca es Apple
+    if marca == "Apple":
+        print(f"{marca} es la mejor marca de teléfonos")
+    else:
+        print(f"{marca} es una buena marca, pero no como Apple")
