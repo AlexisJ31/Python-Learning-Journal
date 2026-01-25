1e7b0baebb6caf8bd2f29ef3ca1cb0e8bbe5cfbc
Alexis <alexisjaen35@gmail.com>
2026-01-24 20:30:47 -0500

    practicas del bucle for, dia 2

---
+++ day2_basics_python/for_practice.py
@@ -0,0 +1,11 @@
+# Crea un archivo llamado for_practice.py en VS Code
+# un bucle for que reccore una lista de jugadores de fútbol del FC Barcelona
+crack_players = ["Messi", "Lamine Yamal", "Pedri"]
+
+for player in crack_players:
+    print(f"{player} es un crack del Barça")
+
+#ejemplo 2
+# Esto imprimirá del 0 al 4 (el 5 no se incluye)
+for i in range(5):
+    print(f"Vuelta número: {i}")
