eec878a0d6649140910af316f7e6ce77c6b71570
Alexis <alexisjaen35@gmail.com>
2026-01-25 14:07:29 -0500

    practicas del dia 3, ciclo while

---
+++ day3_basics_python/while_practice.py
@@ -0,0 +1,7 @@
+#Ejercicio 1 simulador de piques al dia para carrera
+piques_diarios = 0
+meta_piques = 5
+while piques_diarios < meta_piques:
+    piques_diarios += 1
+    print(f"Vamos por el pique número: {piques_diarios}, falta poco para la meta de {meta_piques} piques diarios")
+print("¡Meta de piques diarios alcanzada!")
