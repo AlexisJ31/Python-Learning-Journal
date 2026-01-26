42990ae4cd11f0063c1adc28cc73b61835d526bb
Alexis <alexisjaen35@gmail.com>
2026-01-26 18:02:17 -0500

    dia 4, practica diccionarios

---
+++ practicas_diarias/day4_basics_python/Diccionarios_practice.py
@@ -0,0 +1,9 @@
+#declaramos la varible mi perro con sus llaves y valores
+mi_perro = {
+    "nombre" : "Lana",
+    "edad" : 6,
+    "raza" : "Labrador",
+    "color" : "negro"
+}
+#print para imprimnir el nombre por ejemplo
+print (mi_perro["nombre"])
