c3d85ec1bc967ccc7e475b5d8e9fbbf46b02fddd
Alexis <alexisjaen35@gmail.com>
2026-01-26 18:39:26 -0500

    en el main importamos el diccionario y la funcion para que imprima los datos

---
+++ practicas_diarias/day4_basics_python/main.py
@@ -0,0 +1,5 @@
+#importamos la funcion desde otro archivo
+import Diccionarios_practice
+from funciones_practice import mostrar_ficha
+#llamamos a la funcion y le pasamos el diccionario como argumento
+mostrar_ficha(Diccionarios_practice.mi_perro)
