2dbb9b74274e4316efd6c18b98dc6ab2d05ffccb
Alexis <alexisjaen35@gmail.com>
2026-01-26 18:38:36 -0500

    creamos la funcion donde llama los datos del diccionario y es donde se imprimen

---
+++ practicas_diarias/day4_basics_python/funciones_practice.py
@@ -0,0 +1,7 @@
+#definimos la funcion 
+def mostrar_ficha(datos_perro):
+    print("Ficha del perro:")
+    print("Nombre:", datos_perro["nombre"])
+    print("Edad:", datos_perro["edad"])
+    print("Raza:", datos_perro["raza"])
+    print("Color:", datos_perro["color"])
