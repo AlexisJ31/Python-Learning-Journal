4c64b9c276708648ddd6e08b226f4bbf0cfc9446
Alexis <alexisjaen35@gmail.com>
2026-01-27 00:19:51 -0500

    se le agrego mas funciones

--- practicas_diarias/day4_basics_python/funciones_practice.py
+++ practicas_diarias/day4_basics_python/funciones_practice.py
@@ -5,3 +5,6 @@ def mostrar_ficha(datos_perro):
     print("Edad:", datos_perro["edad"])
     print("Raza:", datos_perro["raza"])
     print("Color:", datos_perro["color"])
+#se pueden colocar mas funciones
+def saludar():
+    print("¡Hola! Bienvenido al programa de gestión de perros.")
--- practicas_diarias/day4_basics_python/main.py
+++ practicas_diarias/day4_basics_python/main.py
@@ -1,5 +1,7 @@
 #importamos la funcion desde otro archivo
 import Diccionarios_practice
 from funciones_practice import mostrar_ficha
+from funciones_practice import saludar
 #llamamos a la funcion y le pasamos el diccionario como argumento
 mostrar_ficha(Diccionarios_practice.mi_perro)
+saludar()
