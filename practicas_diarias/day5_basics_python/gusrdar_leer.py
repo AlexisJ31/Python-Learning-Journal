ff34d73005cf49465c7971692ab03e19ee057450
Alexis <alexisjaen35@gmail.com>
2026-01-28 18:49:26 -0500

    se le actualizo la automatizacion usando os

--- practicas_diarias/day5_basics_python/gusrdar_leer.py
+++ practicas_diarias/day5_basics_python/gusrdar_leer.py
@@ -1,4 +1,6 @@
 import os
+# Esto asegura que el archivo se cree en la misma carpeta que el script
+ruta_archivo = os.path.join(os.path.dirname(__file__), "convocados.txt") #esto mas que nada nos sirve para hacer la automatizacion y para que el robot de github pueda ver el error
 # imprimir la bienvenida a la convocatoria del equipo
 print("¡Bienvenidos a la convocatoria del equipo!")
 # usaremos try except para manejar errores al abrir el archivo
@@ -6,7 +8,7 @@ try:
     # leer que jugador desea convocar
     jugador = input("Ingrese el nombre del jugador que desea convocar: ")
     # Guardar (Modo 'a' de Append para no borrar a los anteriores)
-    with open("convocados.txt", "a") as archivo:
+    with open(ruta_archivo, "a") as archivo:
         archivo.write(jugador + "\n")
 
     #im0primimos si el jugador se gusrdo correctamente
@@ -14,7 +16,7 @@ try:
 
     # Leer el archivo para mostrar la lista de convocados
     print("\nLista de jugadores convocados:")
-    with open("convocados.txt", "r") as archivo:
+    with open(ruta_archivo, "r") as archivo:
         #aqui leemos todas las lineas del archivo
         convocados = archivo.readlines()
         #preguntamos si hay convocados
